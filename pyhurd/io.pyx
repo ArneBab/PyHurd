13de499368d247b33fcf35195d078eb10aaa7c78
Anatoly A. Kazantsev <jim-crow@rambler.ru>
2008-06-15 16:55:07 +0700

    class IO now extending MachPort from mach module

--- pyhurd/io.pyx
+++ pyhurd/io.pyx
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-include "fcntl.pxi"
-include "io.pxi"
+from pyhurd.mach cimport kern_return_t, vm_size_t, mach_msg_type_number_t, mach_port_t, mach_msg_type_name_t, natural_t
 
-O_READ = _O_READ
-O_WRITE = _O_WRITE
-O_CREAT = _O_CREAT
-O_TRUNC = _O_TRUNC
+cdef extern  from "hurd/io.h":
+    kern_return_t io_readable (io_t io_object, vm_size_t * amount)
+    kern_return_t io_read (io_t io_object, data_t * data, mach_msg_type_number_t * dataCnt, loff_t offset, vm_size_t amount)
+    kern_return_t io_write (io_t io_object, data_t data, mach_msg_type_number_t dataCnt, loff_t offset, vm_size_t * amount)
+    kern_return_t io_seek (io_t io_object, loff_t offset, int whence, loff_t * newp)
+    kern_return_t io_set_all_openmodes (io_t io_object, int newbits)
+    kern_return_t io_get_openmodes (io_t io_object, int * bits)
+    kern_return_t io_set_some_openmodes (io_t io_object, int bits_to_set)
+    kern_return_t io_clear_some_openmodes (io_t io_object, int bits_to_clear)
+    kern_return_t io_async (io_t io_object, mach_port_t notify_port, mach_msg_type_name_t notify_portPoly, mach_port_t * async_id_port)
+    kern_return_t io_mod_owner (io_t io_object, pid_t owner)
+    kern_return_t io_get_owner (io_t io_object, pid_t * owner)
+    kern_return_t io_select (io_t io_object, mach_port_t reply, natural_t timeout, int *select_type) 
 
-cdef class IO:
-    cdef readonly io_t io_object
-    
-    def __new__ (self):
-        self.io_object = _MACH_PORT_NULL
-
-    def __dealloc__ (self):
-        mach_port_deallocate(mach_task_self(), self.io_object)
-
+cdef class IO (MachPort):
     def __str__ (self):
-        return 'IO: mach port: %s' % self.io_object
+        return 'IO: mach port: %s' % self.mach_port
 
     def readable (self):
         """
         Checks if io object is readable and return avaiable amount
         of data to read/write
 
-        Return:
+       Return:
             tuple of error and amount integer values
 
         Hurd definition:
@@ -36,7 +36,7 @@ cdef class IO:
         """
 
         cdef vm_size_t amount = 0
-        error = io_readable(self.io_object, &amount)
+        error = io_readable(self.mach_port, &amount)
 
         return error, amount
 
@@ -63,7 +63,7 @@ cdef class IO:
 
         cdef vm_size_t read_amount
         cdef data_t data = <data_t>malloc(amount + 1)
-        error = io_read(self.io_object, &data, &read_amount, offset, amount)
+        error = io_read(self.mach_port, &data, &read_amount, offset, amount)
 
         if read_amount == 0:
             return None
@@ -92,7 +92,7 @@ cdef class IO:
         """
 
         cdef vm_size_t write_amount
-        error = io_write(self.io_object, data, len(data), offset, &write_amount)
+        error = io_write(self.mach_port, data, len(data), offset, &write_amount)
         return error, write_amount
 
     def seek (self, offset, whence):
@@ -111,7 +111,7 @@ cdef class IO:
         """
 
         cdef loff_t new_position
-        error = io_seek(self.io_object, offset, whence, &new_position)
+        error = io_seek(self.mach_port, offset, whence, &new_position)
         return error, new_position
 
     def set_all_openmodes (self, int newbits):
@@ -119,7 +119,7 @@ cdef class IO:
         Return:
              error
         """
-        return io_set_all_openmodes(self.io_object, newbits)
+        return io_set_all_openmodes(self.mach_port, newbits)
 
     def get_openmodes (self):
         """
@@ -128,7 +128,7 @@ cdef class IO:
         """
 
         cdef int bits
-        error = io_get_openmodes(self.io_object, &bits)
+        error = io_get_openmodes(self.mach_port, &bits)
         return error, bits
 
     def set_some_openmodes (self, bits_to_set):
@@ -137,7 +137,7 @@ cdef class IO:
              error
         """
 
-        return io_set_some_openmodes(self.io_object, bits_to_set)
+        return io_set_some_openmodes(self.mach_port, bits_to_set)
 
     def clear_some_openmodes (self, bits_to_clear):
         """
@@ -145,9 +145,8 @@ cdef class IO:
              error
         """
 
-        return io_clear_some_openmodes(self.io_object, bits_to_clear)
+        return io_clear_some_openmodes(self.mach_port, bits_to_clear)
 
-    #Looks like we need some sort of MACH_PORT object :-)
     def async (self, notify_port, notify_portPoly):
         """
         This requests that the IO object send SIGIO and SIGURG signals,
@@ -161,8 +160,12 @@ cdef class IO:
              error, async_id_port
         """
 
-        cdef mach_port_t async_id_port
-        error = io_async(self.io_object, notify_port, notify_portPoly, &async_id_port)
+        cdef mach_port_t _async_id_port
+        error = io_async(self.mach_port, notify_port.mach_port, notify_portPoly, &_async_id_port)
+
+        async_id_port = MachPort()
+        async_id_port.mach_port = _async_id_port
+
         return error, async_id_port
 
     def mod_owner (self, owner):
@@ -178,7 +181,7 @@ cdef class IO:
              error
         """
 
-        return io_mod_owner(self.io_object, owner)
+        return io_mod_owner(self.mach_port, owner)
 
     def get_owner (self):
         """
@@ -194,7 +197,7 @@ cdef class IO:
         """
 
         cdef pid_t owner
-        error = io_get_owner(self.io_object, &owner)
+        error = io_get_owner(self.mach_port, &owner)
         return error, owner
 
     def select (self, reply, timeout, select_type):
@@ -208,5 +211,5 @@ cdef class IO:
         """
 
         cdef int _select_type = select_type
-        error = io_select(self.io_object, reply, timeout, &_select_type)
+        error = io_select(self.mach_port, reply, timeout, &_select_type)
         return error, _select_type
