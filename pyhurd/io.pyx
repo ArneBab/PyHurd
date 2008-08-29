9cf7aefbcd4481bdf1f3d5b59bc8f4310603b9ae
Arne Babenhauserheide <bab@draketo.de>
2008-08-29 09:59:38 +0200

    Added Copyright headers to all *.pyx files.

--- pyhurd/io.pyx
+++ pyhurd/io.pyx
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+PyHurd - A pytonish GNU/Hurd
+'''
+
+__copyright__ = """
+    Copyright © 2008 Anatoly A. Kazantsev <jim-crow@rambler.ru>
+
+    This file is part of PyHurd.
+
+    PyHurd is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PyHurd is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from pyhurd.mach cimport kern_return_t, vm_size_t, mach_msg_type_number_t, mach_port_t, mach_msg_type_name_t, natural_t
 
 cdef extern  from "hurd/io.h":
--- pyhurd/pyhurd.mach.pyx
+++ pyhurd/pyhurd.mach.pyx
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+PyHurd - A pytonish GNU/Hurd
+'''
+
+__copyright__ = """
+    Copyright © 2008 Anatoly A. Kazantsev <jim-crow@rambler.ru>
+
+    This file is part of PyHurd.
+
+    PyHurd is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PyHurd is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 MACH_PORT_NULL = None
 
 cdef class MachPort:
