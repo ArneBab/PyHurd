8eb224ef5a8ca323baec0d23916ef9700d80949e
Anatoly A. Kazantsev <anatoly@gnu.org>
2008-10-04 20:23:03 +0700

    2008-10-04 Anatoly A. Kazantsev <anatoly@gnu.org> * COPYING: New file. * setup.py, copy.py, dump.py: Add licence and copyright notice. * gnu_source.h, io.pyx, pyhurd.fcntl.pxd: Likewise. * pyhurd.fcntl.pyx, pyhurd.glibc.pxd, pyhurd.glibc.pyx: Likewise. * pyhurd.hurd.pxd, pyhurd.hurd.pyx, pyhurd.mach.pxd: Likewise. * pyhurd.mach.pyx, test_io.py: Likewise.

--- pyhurd/pyhurd.fcntl.pyx
+++ pyhurd/pyhurd.fcntl.pyx
@@ -2,26 +2,25 @@
 # -*- coding: utf-8 -*-
 
 '''
-PyHurd - A pytonish GNU/Hurd
+pyHurd - A pytonish GNU/Hurd
 '''
 
 __copyright__ = """
-    Copyright © 2008 Anatoly A. Kazantsev <jim-crow@rambler.ru>
+Copyright (C) 2008 Anatoly A. Kazantsev
 
-    This file is part of PyHurd.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-    PyHurd is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    PyHurd is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
 cdef extern from "fcntl.h":
