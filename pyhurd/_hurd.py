fde7a38f714380cd7fb632923b125f96e0a2db20
Anatoly A. Kazantsev <anatoly@gnu.org>
2009-01-03 00:59:15 +0600

    * _glibc.pxd, _glibc.py: Add 2009 year to copyright. _hurd.pxd, _hurd.py, _mach.py, hurd.py: Likewise.

--- pyhurd/_hurd.py
+++ pyhurd/_hurd.py
@@ -6,7 +6,7 @@ pyHurd - A pytonish GNU/Hurd
 '''
 
 __copyright__ = '''
-Copyright (C) 2008 Anatoly A. Kazantsev
+Copyright (C) 2008, 2009 Anatoly A. Kazantsev
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
